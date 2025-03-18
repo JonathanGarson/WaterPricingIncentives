16acb029bfc32a6ecd5581b191056b3a4a0bab5a
MathildeB9 <mathblanchon@gmail.com>
2025-03-18 21:24:13 +0100

    table 5 clean

--- replication_R/table_5.R
+++ replication_R/table_5.R
@@ -7,8 +7,15 @@ library(modelsummary)
 library(haven)
 library(car)
 
-# Constructing baseline_covariates dataset (derived) -----------------------
+# Function ----------------------------------------------------------------
 
+p_value_joint_test = function(dataset, test_var){
+  hyp_test = linearHypothesis(dataset, test_var, test = "F")
+  p_value = hyp_test$`Pr(>F)`[2]
+  return(p_value)
+}
+
+# Constructing baseline_covariates dataset (derived) -----------------------
 dt_baseline <- fread("C:\\Users\\Dell\\replication_package\\data\\input_primary\\rct1_baseline.csv")
 
 # calculate boro rice revenue per acre
@@ -174,28 +181,20 @@ t_dmean_vars <- grep("^t_dmean_", names(dt), value = TRUE)
 rhs <- paste(c("treatment", "anymarginal", "t_marg", dmean_vars, t_dmean_vars), collapse = " + ")
 formula_str <- paste("waterlevel ~", rhs, "| Upazila")
 formula <- as.formula(formula_str)
-mod <- feols(formula, data = dt, cluster = "village_id")
+reg_1 <- feols(formula, data = dt, cluster = "village_id")
 
-lh_test <- linearHypothesis(mod, "treatment + t_marg = 0")
-p_value <- lh_test[2, "Pr(>Chisq)"]
+# Wald test
+test <- linearHypothesis(reg_1, "treatment + t_marg = 0")
+p_value <- test[2, "Pr(>Chisq)"]
 
-# Calcul immédiat de la moyenne du groupe contrôle
-vars_used <- c("waterlevel", "treatment", "anymarginal", "t_marg", dmean_vars, t_dmean_vars, "upazila_id", "village_id")
-dt_model_geo <- dt[complete.cases(dt[, ..vars_used])]
-mean_control_geo <- dt_model_geo[treatment == 0, mean(waterlevel, na.rm = TRUE)]
-cat("Moyenne du groupe contrôle (waterlevel):", mean_control_geo, "\n")
-
-# Y a-t-il des données pour chaque groupe de traitement après filtrage?
-dt_diagnostics <- dt[complete.cases(dt[, ..vars_needed])]
-table(dt_diagnostics$treatment)
-
+# Control mean
+control_mean = round(dt[treatment == 0, .(mean = mean(waterlevel))], 2)$mean
 
 
 wlevel_rob <- list(
-  model = mod,
+  model = reg_1,
   heterogeneity_p_value = p_value,
-  mean_control = mean_control,
-  test_result = lh_test
+  mean_control = control_mean
 )
 
 cat("=== Résultats pour wlevel_rob ===\n")
@@ -215,43 +214,22 @@ rhs_vars <- c("treatment", "anymarginal", "t_marg",
 rhs <- paste(rhs_vars, collapse = " + ")
 formula_str <- paste("waterlevel ~", rhs, "| Upazila")
 formula <- as.formula(formula_str)
-mod <- feols(formula, data = dt_model, cluster = "village_id")
+reg_2 <- feols(formula, data = dt, cluster = "village_id")
 
-# Calcul de la moyenne du groupe de controle
-dt_model <- copy(dt)
-vars_model <- c("waterlevel", "treatment", "anymarginal", "t_marg", 
-                dmean_vars_general, t_dmean_vars_general, dmeanGeo_vars, t_dmeanGeo_vars, "upazila")
-dt_model <- dt[complete.cases(dt[, ..vars_model])]
-mod2 <- feols(formula, data = dt_model, cluster = "village_id")
-mean_control <- dt_model[treatment == 0, mean(waterlevel, na.rm = TRUE)]
-cat("Moyenne du groupe contrôle (waterlevel) :", mean_control, "\n")
+# Wald test
+test <- linearHypothesis(reg_2, "treatment + t_marg = 0")
+p_value <- test[2, "Pr(>Chisq)"]
 
-# Extraire le résumé du modèle
-s <- summary(mod)
-global_R2 <- s$r2
-adj_R2 <- s$adj.r2
-within_R2 <- s$r2.within 
-
-# Test de l'hypothèse : treatment + t_marg = 0 à l'aide de linearHypothesis()
-lh_test <- linearHypothesis(mod, "treatment + t_marg = 0")
-p_value <- lh_test[2, "Pr(>Chisq)"]
+# Control mean
+control_mean = round(dt[treatment == 0, .(mean = mean(waterlevel))], 2)$mean
 
 
-# Stockage des résultats dans une liste, simulant 'estimates store wlevel_rob_geo'
+# Stock results
 wlevel_rob_geo <- list(
-  model = mod,
-  adj_R2 = adj_R2,
-  within_R2 = within_R2,
+  model = reg_2,
   heterogeneity_p_value = p_value,
-  mean_control = mean_control,
-  test_result = lh_test
+  mean_control = control_mean
 )
-
-# Print qummary to check results
-cat("=== Résultats du modèle wlevel_rob_geo ===\n")
-print(summary(wlevel_rob_geo$model))
-cat("\nMoyenne du groupe contrôle (waterlevel) :", wlevel_rob_geo$mean_control, "\n")
-cat("P-value du test (treatment + t_marg = 0) :", wlevel_rob_geo$heterogeneity_p_value, "\n")
 
 # Regression upasoil_soilclay, upasoil_soilsand, upasoil_soilcarbon, upasoil_soilwater
 
@@ -272,21 +250,18 @@ for(k in geo_vars) {
   formula_str <- paste("waterlevel ~", rhs, "| Upazila")
   formula <- as.formula(formula_str)
   
-  cat("Formule utilisée pour", k, ":\n", formula_str, "\n\n")
-  
   # Model estimation
   mod <- feols(formula, data = dt, cluster = "village_id")
   
   # Test hypothesis: treatment + t_marg = 0
-  lh_test <- linearHypothesis(mod, "treatment + t_marg = 0")
-  p_value <- lh_test[2, "Pr(>Chisq)"]
+  test <- linearHypothesis(mod, "treatment + t_marg = 0")
+  p_value <- test[2, "Pr(>Chisq)"]
   
   # Compute mean waterlevel for the controm group (treatment == 0)
-  mean_control <- dt_model[treatment == 0, mean(waterlevel, na.rm = TRUE)]
+  mean_control = round(dt[treatment == 0, .(mean = mean(waterlevel))], 2)$mean
   
   upasoil_results[[paste0("upasoil_", k)]] <- list(
     model = mod,
-    heterogeneity_test = lh_test,
     heterogeneity_p_value = p_value,
     mean_control = mean_control
   )
@@ -295,12 +270,6 @@ for(k in geo_vars) {
   cat("Moyenne du groupe contrôle (waterlevel) :", mean_control, "\n")
   cat("P-value du test (treatment + t_marg = 0) :", p_value, "\n\n")
 }
-
-# Print summaries to check the results
-print(summary(upasoil_results[["upasoil_soilsand"]][["model"]]))
-print(summary(upasoil_results[["upasoil_soilclay"]][["model"]]))
-print(summary(upasoil_results[["upasoil_soilcarbon"]][["model"]]))
-print(summary(upasoil_results[["upasoil_soilwater"]][["model"]]))
 
 # Create the table as a tex file -------------------------------------------
 models <- list(
@@ -314,31 +283,47 @@ models <- list(
 
 # 2. Add rows for  "Mean in Control" (waterlevel mean when treatment == 0)  an "p-Value: Treat+Treat*Volumetric"
 add_rows_df <- data.frame(
-  term = c("Mean in Control", "p-Value: Treat+Treat*Volumetric"),
-  wlevel_rob = c(wlevel_rob$mean_control, wlevel_rob$heterogeneity_p_value),
-  wlevel_rob_geo = c(wlevel_rob_geo$mean_control, wlevel_rob_geo$heterogeneity_p_value),
-  upasoil_soilclay = c(upasoil_results[["upasoil_soilclay"]][["mean_control"]],
+  term = c("Upazila Fixed Effects", "Controls", "Controls × Treatment", 
+           "Geo Controls", "Geo Controls × Treatment","Mean in Control", "p-Value: Treat+Treat x Volumetric"),
+  wlevel_rob = c("Yes", "Yes", "Yes", "No", "No",wlevel_rob$mean_control, wlevel_rob$heterogeneity_p_value),
+  wlevel_rob_geo = c("Yes", "Yes", "Yes", "Yes", "Yes",wlevel_rob_geo$mean_control, wlevel_rob_geo$heterogeneity_p_value),
+  upasoil_soilclay = c("Yes", "Yes", "Yes", "No", "No",upasoil_results[["upasoil_soilclay"]][["mean_control"]],
                         upasoil_results[["upasoil_soilclay"]][["heterogeneity_p_value"]]),
-  upasoil_soilsand = c(upasoil_results[["upasoil_soilsand"]][["mean_control"]],
+  upasoil_soilsand = c("Yes", "Yes", "Yes", "No", "No",upasoil_results[["upasoil_soilsand"]][["mean_control"]],
                         upasoil_results[["upasoil_soilsand"]][["heterogeneity_p_value"]]),
-  upasoil_soilcarbon = c(upasoil_results[["upasoil_soilcarbon"]][["mean_control"]],
+  upasoil_soilcarbon = c("Yes", "Yes", "Yes", "No", "No",upasoil_results[["upasoil_soilcarbon"]][["mean_control"]],
                          upasoil_results[["upasoil_soilcarbon"]][["heterogeneity_p_value"]]),
-  upasoil_soilwater = c(upasoil_results[["upasoil_soilwater"]][["mean_control"]],
+  upasoil_soilwater = c("Yes", "Yes", "Yes", "No", "No",upasoil_results[["upasoil_soilwater"]][["mean_control"]],
                         upasoil_results[["upasoil_soilwater"]][["heterogeneity_p_value"]])
 )
 
+# Créer le second data.frame avec les informations sur les effets fixes et contrôles
+
+
 # Create LaTex Table
 options("modelsummary_format_numeric_latex" = "plain")
+gof_custom <- tibble::tribble(
+  ~raw, ~clean, ~fmt,
+  "r.squared", "R²", "%.3f"
+)
 tbl <- modelsummary(models,
    statistic = c("{estimate}", "({std.error})"),
-   stars = c(`*` = 0.10, `**` = 0.05, `***` = 0.01),
    add_rows = add_rows_df,
-   coef_omit = "^_cons$",
+   coef_omit = "^(_cons$|dmean_|t_dmean_|dmeanGeo_|t_dmeanGeo_)",
    output = "latex",
    coef_order = c("treatment", "t_marg", "anymarginal", 
                   "t_dupameanGeo_soilclay", "t_dupameanGeo_soilsand", 
                   "t_dupameanGeo_soilcarbon", "t_dupameanGeo_soilwater"),
-   notes = "Upazila Fixed Effects = _IUpa*; Controls = dmean_*; Controls X Treatment = t_dmean_*; Geo Controls = dmeanGeo*; Geo Controls X Treatment = t_dmeanGeo_*",
+   coef_map = c(
+      "treatment" = "Treatment",
+      "t_marg" = "Treatment x Volumetric Pricing",
+      "anymarginal" = "Volumetric Pricing",
+      "t_dupameanGeo_soilclay" = "Soil Clay Content",
+      "t_dupameanGeo_soilsand" = "Soil Sand Content",
+      "t_dupameanGeo_soilcarbon" = "Soil Carbon Content",
+      "t_dupameanGeo_soilwater" = "Soil Water Content"),
+   gof_map = gof_custom,
+   title = "Robustness of Water-Usage Results to Interactions between the AWD Treatment and Covariates",
    file = NULL  
 )
 
